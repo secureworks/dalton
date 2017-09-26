be8e181c7ab3b64e17527843071baaa1318289ef
counterthreatunit <counterthreatunit@users.noreply.github.com>
2017-09-25 22:27:05 -0400

    added license blurb to files

--- app/static/engine-configs/clean_snort_config.py
+++ app/static/engine-configs/clean_snort_config.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python
 
+# Copyright 2017 Secureworks
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import sys
 
-# take a default Snort config file and clean
-# it up so the Dalton Agents won't throw
-# errors. Also tweak it to my preferences.
+"""
+Takes a default Snort config file and clean
+it up so the Dalton Agents won't throw
+ errors. Also tweak it to my preferences.
+"""
 
 def usage_and_exit(msg=None):
     if msg:
@@ -40,7 +56,7 @@ while True:
             continue
         if clean_line.startswith("preprocessor reputation:"):
             # comment out reputation preprocessor
-            #  (black/white list file don't exist and Snort will error
+            #  (black/white list file don't exist and Snort will error)
             while clean_line.endswith("\\"):
                 line = "# %s" % line
                 fh.write(line)
