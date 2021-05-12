9bc5f93df3c5dfd7e04c69bc8e8da506d675e926
Donald Campbell <dcampbell@secureworks.com>
2021-05-12 15:32:24 -0400

    Lua config dynamically updated. Suricata configs restored. README updated

--- app/dalton.py
+++ app/dalton.py
@@ -1710,6 +1710,17 @@ def page_coverage_summary():
                         config['profiling']['keywords'] = {'enabled': True, \
                                                            'filename': "dalton-keyword_perf.log", \
                                                            'append': True}
+
+                if bGetBufferDumps:
+                    buff_dump_config = {'lua': {'enabled': True, \
+                                        'scripts-dir': "/opt/dalton-agent", \
+                                        'scripts': ["http.lua","tls.lua","dns.lua"]}}
+
+                    if 'lua' in olist: # someone added something
+                        config['outputs'][olist.index('lua')] = buff_dump_config
+                    else:
+                        config['outputs'].append(buff_dump_config)
+
                 # write out
                 engine_conf_file = os.path.join(TEMP_STORAGE_PATH, f"{job_id}_suricata.yaml")
                 engine_conf_fh = open(engine_conf_file, "w")
